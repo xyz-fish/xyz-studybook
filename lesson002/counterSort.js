5db2821ea7314f31f54506d007b131339dee7386
songyang <610467559@qq.com>
2021-11-10 12:24:49 +0800

    feat: add counterSort other fn

--- lesson002/counterSort.js
+++ lesson002/counterSort.js
@@ -20,8 +20,56 @@ function counterSort(arr) {
       counter[i]--
     }
   }
+  console.log(arr)
+  return arr
+}
 
+// 从min 开始
+function counterSort2(arr) {
+  var max = Math.max.apply(null, arr)
+  var min = Math.min.apply(null, arr)
+
+  var counter = Array.from({ length: max - min + 1 })
+
+  for (let i = 0; i < arr.length; i++) {
+    var counterIndex = arr[i]
+    if (!counter[counterIndex]) counter[counterIndex] = 0
+    counter[counterIndex]++
+  }
+
+  let index = 0
+
+  for (let j = min; j <= max; j++) {
+    while (counter[j] > 0) {
+      arr[index++] = j
+      counter[j]--
+    }
+  }
+
+  console.log(arr)
+  return arr
+}
+
+function counterSort3(arr) {
+  const max = Math.max.apply(null, arr)
+  const min = Math.min.apply(null, arr)
+
+  const counter = new Map()
+  for (let i = 0; i < arr.length; i++) {
+    var counterIndex = arr[i]
+    counter.set(counterIndex, counter.get(counterIndex) || 0 + 1)
+  }
+
+  let index = 0
+  for (j of counter) {
+    while (j[1] > 0) {
+      arr[index++] = j[0]
+      j[1]--
+    }
+  }
   return arr
 }
 
 counterSort([1, 2, 4, 6, 9, 2, 3, -1, -2])
+counterSort2([1, 2, 4, 6, 9, 2, 3, -1, -2])
+counterSort3([1, 2, 4, 6, 9, 2, 3, -1, -2])
