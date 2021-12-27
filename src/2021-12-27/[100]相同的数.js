19c2d8e24b852ebd6b573375b69de57f83b9d6c4
songyang <610467559@qq.com>
2021-12-27 17:56:45 +0800

    feat: 相同的数

---
+++ src/2021-12-27/[100]相同的数.js
@@ -0,0 +1,17 @@
+function isSameTree(p, q) {
+  if (p === null && q === null) return true
+
+  // p 和 q 节点val 相同的时候 取反 return false
+  // 其他情况继续判断
+  if (!(p !== null && q !== null && q.val === p.val)) return false
+
+  const leftSame = isSameTree(p.left, q.left)
+
+  if (!leftSame) return false
+
+  const rightSame = isSameTree(p.right, q.right)
+
+  if (!rightSame) return false
+
+  return true
+}
