06d9ef5b561ebbef33fd375f1a85593ea5919b60
songyang <610467559@qq.com>
2022-01-04 11:09:28 +0800

    fix: 修改了下insertSortnsertSort

--- lesson001/insertSort.js
+++ lesson001/insertSort.js
@@ -1,8 +1,8 @@
 function insertSort(arr) {
   const len = arr.length
 
-  let cur = 0
-  for (let i = 0; i < len; i++) {
+  let cur = 1
+  for (let i = 1; i < len; i++) {
     cur = i
     // 这里 cur - 1 >=0 的也可以不写  arr[-1] = undefined js 中 小于任何数字 即使是无限小
     while (cur - 1 >= 0 && arr[cur] < arr[cur - 1]) {
